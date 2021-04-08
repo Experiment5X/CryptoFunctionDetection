6f4cf307357740e97f3b60baf90dd53f5c5691fe
Adam <xmeadamx@gmail.com>
2021-04-07 20:41:00 -0400

    Modify code to load all non crypto functions as training code

--- asm2vec/examples/training-estimating.py
+++ asm2vec/examples/training-estimating.py
@@ -2,26 +2,55 @@ import sys
 
 sys.path.append('/Users/adamspindler/Developer/DeepLearningSecurity/Project/asm2vec')
 
+import os
 import numpy as np
+from importlib import import_module
+from pathlib import Path
 
 import asm2vec.asm
 import asm2vec.parse
 import asm2vec.model
 
-from examples.crypto_code.rsa_botan_functions import function_names as func_names
+from examples.crypto_code.rsa.rsa_botan_functions import function_names as func_names
+import examples.non_crypto_code
 
 
 def cosine_similarity(v1, v2):
     return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
 
 
+def get_training_functions():
+    directory_path = './non_crypto_code/'
+    functions = []
+
+    for file_name in os.listdir(directory_path):
+        file_path_str = os.path.join(directory_path, file_name)
+        file_path = Path(file_path_str)
+
+        if file_path.suffix == '.py' and file_name != '__init__.py':
+            package_name = 'examples.non_crypto_code.' + file_path.stem
+            python_package = import_module(package_name)
+
+            try:
+                for optimization_level in range(0, 3):
+                    base_code_name = file_name.replace('_functions.py', '')
+                    assembly_file_name = f'{base_code_name}-o{optimization_level}.s'
+                    assembly_file_path = os.path.join(directory_path, assembly_file_name)
+
+                    parsed_functions = asm2vec.parse.parse(
+                        assembly_file_path, func_names=python_package.function_names
+                    )
+
+                    functions.extend(parsed_functions)
+            except:
+                print(f'Could not load: {base_code_name}')
+    
+    return functions
+
 def main():
-    # func_names = ['_rsa_encrypt_key_ex']
-    training_funcs = asm2vec.parse.parse(
-        'crypto_code/rsa_botan-o0.s', func_names=func_names
-    )
+    training_funcs = get_training_functions()
     estimating_funcs = asm2vec.parse.parse(
-        'crypto_code/rsa_botan-o1.s', func_names=func_names
+        'crypto_code/rsa/rsa_botan-o1.s', func_names=func_names
     )
 
     print('# of training functions:', len(training_funcs))
@@ -51,6 +80,8 @@ def main():
         for (ef, efv) in zip(estimating_funcs, estimating_funcs_vec):
             sim = cosine_similarity(tf.v, efv)
             print('sim("{}", "{}") = {}'.format(tf.sequential().name(), ef.name(), sim))
+        
+        break
 
 
 if __name__ == '__main__':
